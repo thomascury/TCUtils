24263c8b7da5caf813bbc804a51c022fef9d0ba7
thomas.cury <thomas@cury.fr>
2016-02-11 15:14:43 +0100

    Added dice roll lib Added diceware password generation

---
+++ tcutils/dice.py
@@ -0,0 +1,12 @@
+from os import urandom
+import sys
+
+
+def roll(i):
+    return tuple(r % 6 + 1 for r in urandom(i))
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        print(roll(int(sys.argv[1])))
+    else:
+        print(roll(1))
